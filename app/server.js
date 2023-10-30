683bf30e7a3fe6092faee405c6a3cdc5a72fba46
JonathanOHolmberg <127875373+JonathanOHolmberg@users.noreply.github.com>
2023-10-30 08:22:51 +0200

    mvp

--- app/server.js
+++ app/server.js
@@ -1,9 +1,24 @@
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('../db/shoppinglist.db');
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from the Node.js server!');
+  try {
+    db.all('SELECT * FROM item', (err, rows) => {
+      if (err) {
+        console.error(err.message);
+        res.status(500).json({ error: err.message });
+      } else {
+        console.log('Data from the shopping list:', rows);
+        res.json(rows);
+      }
+    });
+  } catch (e) {
+    console.error(e.message);
+    res.status(500).json({ error: e.message });
+  }
 });
 
 app.listen(port, () => {
